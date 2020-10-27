6135006169df5e8b08c94f83a0c9962ce3326457
scpcom <somebody@example.com>
2020-10-27 22:43:02 +0100

    USBCDC: examples/USB_UART_BRIDGE: remove duplicate delay_ms

--- libraries/USBCDC/examples/USB_UART_BRIDGE/USB_UART_BRIDGE.ino
+++ libraries/USBCDC/examples/USB_UART_BRIDGE/USB_UART_BRIDGE.ino
@@ -232,22 +232,6 @@ static void uart_wait_sent(void)
     }
 }
 
-//===========================
-void delay_ms(uint32_t count)
-{
-	uint64_t start_mtime, delta_mtime;
-
-	// Don't start measuruing until we see an mtime tick
-	uint64_t tmp = get_timer_value();
-	do {
-	    start_mtime = get_timer_value();
-	} while (start_mtime == tmp);
-
-	do {
-	    delta_mtime = get_timer_value() - start_mtime;
-	} while(delta_mtime <(SystemCoreClock/4000.0 *count ));
-}
-
 //============
 void setup()
 {
