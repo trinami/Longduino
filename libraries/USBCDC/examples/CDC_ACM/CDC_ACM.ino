8550368aeb1ad594eb17c977fbc3b92ea92e6fc8
scpcom <somebody@example.com>
2020-11-02 02:13:30 +0100

    USBCDC: add CDC_ACM example

---
+++ libraries/USBCDC/examples/CDC_ACM/CDC_ACM.ino
@@ -0,0 +1,141 @@
+/*!
+    \file  main.c
+    \brief USB CDC ACM device
+
+    \version 2019-6-5, V1.0.0, firmware for GD32VF103
+*/
+
+/*
+    Copyright (c) 2019, GigaDevice Semiconductor Inc.
+
+    Redistribution and use in source and binary forms, with or without modification, 
+are permitted provided that the following conditions are met:
+
+    1. Redistributions of source code must retain the above copyright notice, this 
+       list of conditions and the following disclaimer.
+    2. Redistributions in binary form must reproduce the above copyright notice, 
+       this list of conditions and the following disclaimer in the documentation 
+       and/or other materials provided with the distribution.
+    3. Neither the name of the copyright holder nor the names of its contributors 
+       may be used to endorse or promote products derived from this software without 
+       specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
+IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
+INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT 
+NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR 
+PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
+WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY 
+OF SUCH DAMAGE.
+*/
+
+
+#include "drv_usb_hw.h"
+#include "cdc_acm_core.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+usb_core_driver USB_OTG_dev = 
+{
+    .dev = {
+        .desc = {
+            .dev_desc       = (uint8_t *)&device_descriptor,
+            .config_desc    = (uint8_t *)&configuration_descriptor,
+            .strings        = usbd_strings,
+        }
+    }
+};
+
+void cdc_acm_set_control_line_state(uint8_t rts, uint8_t dtr)
+{
+  
+}
+
+uint8_t cdc_acm_send_break(uint8_t brk)
+{
+    return brk;
+}
+
+void cdc_acm_set_line_coding(line_coding_struct* linecoding)
+{
+  
+}
+
+uint8_t cdc_acm_custom_req_handler(usb_dev *pudev, usb_req *req)
+{
+  
+}
+
+/*!
+    \brief      receive CDC ACM data
+    \param[in]  pudev: pointer to USB device instance
+    \param[out] none
+    \retval     USB device operation status
+*/
+void cdc_acm_data_receive(usb_dev *pudev)
+{
+    cdc_acm_packet_receive = 0;
+
+    usbd_ep_recev(pudev, CDC_ACM_DATA_OUT_EP, cdc_acm_get_cmdbuf(), CDC_ACM_DATA_PACKET_SIZE);
+}
+
+/*!
+    \brief      send CDC ACM data
+    \param[in]  pudev: pointer to USB device instance
+    \param[out] none
+    \retval     USB device operation status
+*/
+void cdc_acm_data_send (usb_dev *pudev, uint32_t data_len)
+{
+    /* limit the transfer data length */
+    if (data_len <= CDC_ACM_DATA_PACKET_SIZE) {
+        cdc_acm_packet_sent = 0;
+        usbd_ep_send(pudev, CDC_ACM_DATA_IN_EP, cdc_acm_get_cmdbuf(), data_len);
+    }
+}
+
+/*!
+    \brief      main routine will construct a USB keyboard
+    \param[in]  none
+    \param[out] none
+    \retval     none
+*/
+int main(void)
+{
+    eclic_global_interrupt_enable();	
+
+    eclic_priority_group_set(ECLIC_PRIGROUP_LEVEL2_PRIO2);
+
+    usb_rcu_config();
+
+    usb_timer_init();
+
+    usb_intr_config();
+
+    usbd_init (&USB_OTG_dev, USB_CORE_ENUM_FS, &usbd_cdc_cb);
+
+    /* check if USB device is enumerated successfully */
+    while (USBD_CONFIGURED != USB_OTG_dev.dev.cur_status) {
+    }
+
+    while (1) {
+        if (USBD_CONFIGURED == USB_OTG_dev.dev.cur_status) {
+            if (1 == cdc_acm_packet_receive && 1 == cdc_acm_packet_sent) {
+                cdc_acm_packet_sent = 0;
+                /* receive datas from the host when the last packet datas have sent to the host */
+                cdc_acm_data_receive(&USB_OTG_dev);
+            } else {
+                if (0 != cdc_acm_receive_length) {
+                    /* send receive datas */
+                    cdc_acm_data_send(&USB_OTG_dev, cdc_acm_receive_length);
+                    cdc_acm_receive_length = 0;
+                }
+            }
+        }
+    }
+}
