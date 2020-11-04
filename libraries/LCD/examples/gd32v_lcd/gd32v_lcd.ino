13940d27e5a9e29e26fe642fccfa2c7fa4716161
scpcom <somebody@example.com>
2020-11-04 23:28:12 +0100

    LCD: examples/gd32v_lcd: update

--- libraries/LCD/examples/gd32v_lcd/gd32v_lcd.ino
+++ libraries/LCD/examples/gd32v_lcd/gd32v_lcd.ino
@@ -2,50 +2,19 @@
 #include "tf_card.h"
 #include <string.h>
 
+uint8_t mount_is_ok = 1; /* 0: mount successful ; 1: mount failed */
 unsigned char image[12800];
 FATFS fs;
 
-void init_uart0(void)
-{	
-	/* enable GPIO clock */
-    rcu_periph_clock_enable(RCU_GPIOA);
-    /* enable USART clock */
-    rcu_periph_clock_enable(RCU_USART0);
-
-    /* connect port to USARTx_Tx */
-    gpio_init(GPIOA, GPIO_MODE_AF_PP, GPIO_OSPEED_50MHZ, GPIO_PIN_9);
-    /* connect port to USARTx_Rx */
-    gpio_init(GPIOA, GPIO_MODE_IN_FLOATING, GPIO_OSPEED_50MHZ, GPIO_PIN_10);
-
-	/* USART configure */
-    usart_deinit(USART0);
-    usart_baudrate_set(USART0, 115200U);
-    usart_word_length_set(USART0, USART_WL_8BIT);
-    usart_stop_bit_set(USART0, USART_STB_1BIT);
-    usart_parity_config(USART0, USART_PM_NONE);
-    usart_hardware_flow_rts_config(USART0, USART_RTS_DISABLE);
-    usart_hardware_flow_cts_config(USART0, USART_CTS_DISABLE);
-    usart_receive_config(USART0, USART_RECEIVE_ENABLE);
-    usart_transmit_config(USART0, USART_TRANSMIT_ENABLE);
-    usart_enable(USART0);
-
-    usart_interrupt_enable(USART0, USART_INT_RBNE);
-}
-
-int main(void)
+void setup()
 {
-    uint8_t mount_is_ok = 1; /* 0: mount successful ; 1: mount failed */
-    int offset = 0;
-    FIL fil;
     FRESULT fr;     /* FatFs return code */
-    UINT br;
 
+    /* init RGB LED */
     rcu_periph_clock_enable(RCU_GPIOA);
     rcu_periph_clock_enable(RCU_GPIOC);
     gpio_init(GPIOC, GPIO_MODE_OUT_PP, GPIO_OSPEED_50MHZ, GPIO_PIN_13);
     gpio_init(GPIOA, GPIO_MODE_OUT_PP, GPIO_OSPEED_50MHZ, GPIO_PIN_1|GPIO_PIN_2);
-
-    init_uart0();
 
     Lcd_Init();			// init OLED
     LCD_Clear(WHITE);
@@ -61,10 +30,25 @@ int main(void)
     else
         mount_is_ok = 1;
 
+    if (mount_is_ok != 0)
+    {
+        LCD_ShowString(24,  0, (u8 *)("no card found!"), BLACK);
+        LCD_ShowString(24, 16, (u8 *)("no card found!"), BLUE);
+        LCD_ShowString(24, 32, (u8 *)("no card found!"), BRED);
+        LCD_ShowString(24, 48, (u8 *)("no card found!"), GBLUE);
+        LCD_ShowString(24, 64, (u8 *)("no card found!"), RED);
+    }
+}
+
+void loop()
+{
+    int offset = 0;
+    FIL fil;
+    FRESULT fr;     /* FatFs return code */
+    UINT br;
+
     if (mount_is_ok == 0)
     {
-        while(1)
-        {
             offset = 0;
             fr = f_open(&fil, "logo.bin", FA_READ);
             if (fr) printf("open error: %d!\n\r", (int)fr);
@@ -100,24 +84,15 @@ int main(void)
 
             /* Close the file */
             f_close(&fil);
-        }
     }
     else
     {
-        LCD_ShowString(24,  0, (u8 *)("no card found!"), BLACK);
-        LCD_ShowString(24, 16, (u8 *)("no card found!"), BLUE);
-        LCD_ShowString(24, 32, (u8 *)("no card found!"), BRED);
-        LCD_ShowString(24, 48, (u8 *)("no card found!"), GBLUE);
-        LCD_ShowString(24, 64, (u8 *)("no card found!"), RED);
-        while (1)
-        {
             LEDR_TOG;
             delay_1ms(200);
             LEDG_TOG;
             delay_1ms(200);
             LEDB_TOG;
             delay_1ms(200);
-        }
     }
 }
 
