b95821f9b99c13c6a96b6c8531492a17a57e3244
scpcom <scpcom@gmx.de>
2020-11-22 00:17:47 +0100

    core: Tone: add more comments

--- cores/arduino/Tone.cpp
+++ cores/arduino/Tone.cpp
@@ -69,7 +69,6 @@ void tone(pin_size_t pinNumber, unsigned int frequency, unsigned long duration)
     timer_struct_para_init(&timer_initpara);
     /* TIMER configuration */
     timer_initpara.prescaler = prescaler - 1;
-
     timer_initpara.alignedmode = TIMER_COUNTER_EDGE;
     timer_initpara.counterdirection = TIMER_COUNTER_UP;
     timer_initpara.period = period;
@@ -86,22 +85,23 @@ void tone(pin_size_t pinNumber, unsigned int frequency, unsigned long duration)
     timer_ocinitpara.ocnpolarity  = TIMER_OCN_POLARITY_HIGH;
     timer_ocinitpara.ocidlestate  = TIMER_OC_IDLE_STATE_LOW;
     timer_ocinitpara.ocnidlestate = TIMER_OCN_IDLE_STATE_LOW;
-
     timer_channel_output_config(PIN_MAP[pinNumber].timer_device->timer_dev,
         PIN_MAP[pinNumber].timer_channel, &timer_ocinitpara);
 
+    /* channel configuration in PWM mode, duty cycle 50% */
     timer_channel_output_pulse_value_config(
         PIN_MAP[pinNumber].timer_device->timer_dev,
         PIN_MAP[pinNumber].timer_channel, value);
-
     timer_channel_output_mode_config(PIN_MAP[pinNumber].timer_device->timer_dev,
         PIN_MAP[pinNumber].timer_channel, TIMER_OC_MODE_PWM0);
     timer_channel_output_shadow_config(
         PIN_MAP[pinNumber].timer_device->timer_dev,
         PIN_MAP[pinNumber].timer_channel, TIMER_OC_SHADOW_DISABLE);
 
+    /* auto-reload preload enable */
     timer_auto_reload_shadow_enable(PIN_MAP[pinNumber].timer_device->timer_dev);
     _start_time = millis();
+    /* TIMER counter enable */
     timer_enable(PIN_MAP[pinNumber].timer_device->timer_dev);
     if (duration > 0) {
         while ( millis() - _start_time < duration );
