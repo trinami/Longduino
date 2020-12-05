e71ffa73104de683ac396cbfcf5242c1e953b542
scpcom <scpcom@gmx.de>
2020-12-05 17:37:43 +0100

    variants: sipeed-longan-nano: add SPI_MAP

--- variants/sipeed-longan-nano/pins_arduino.h
+++ variants/sipeed-longan-nano/pins_arduino.h
@@ -84,6 +84,7 @@ extern "C" {
 
 /* GD32VF103CBT6 package LQFP48 (PC0-PC12 n/a, PD0/PD1 not used here) */
 #define VARIANT_GPIO_NUM (48)
+#define VARIANT_SPI_NUM (3)
 #define VARIANT_TIMER_NUM (5)
 
 /* Pin aliases: these give the GPIO port/bit for each pin as an
@@ -125,6 +126,15 @@ typedef struct _spi_dev_t {
     rcu_periph_enum clk_id;
 }spi_dev_t;
 
+typedef struct _spi_map_t
+{
+    spi_dev_t * spi_device;
+    uint8_t nss_pin;
+    uint8_t sck_pin;
+    uint8_t miso_pin;
+    uint8_t mosi_pin;
+} spi_map_t;
+
 typedef struct _gd32v_pin_info_t
 {
     gpio_dev_t * gpio_device;
@@ -137,15 +147,17 @@ typedef struct _gd32v_pin_info_t
 } gd32v_pin_info_t;
 
 extern const gd32v_pin_info_t PIN_MAP[VARIANT_GPIO_NUM];
+extern const spi_map_t SPI_MAP[VARIANT_SPI_NUM];
 extern const timer_dev_t * TIMER_MAP[VARIANT_TIMER_NUM];
 
 #define digitalPinToPort(p) ((PIN_MAP[p].gpio_device)->gpio_port)
 #define digitalPinToBitMask(p) (BIT(PIN_MAP[p].gpio_bit))
 #define digitalPinToClkid(p) (PIN_MAP[p].gpio_device->clk_id)
 
-#define digitalPinSPIAvailiable(p) (PIN_MAP[p].spi_device != 0)
-#define digitalPinToSPIDevice(p) (PIN_MAP[p].spi_device->spi_dev)
-#define digitalPinToSPIClockId(p) (PIN_MAP[p].spi_device->clk_id)
+spi_dev_t * digitalPinToSPIInfo(uint8_t pinNumber);
+#define digitalPinSPIAvailiable(p) (digitalPinToSPIInfo(p) != 0)
+#define digitalPinToSPIDevice(p) (digitalPinToSPIInfo(p)->spi_dev)
+#define digitalPinToSPIClockId(p) (digitalPinToSPIInfo(p)->clk_id)
 
 
 #define VARIANT_GPIO_OSPEED GPIO_OSPEED_50MHZ //
